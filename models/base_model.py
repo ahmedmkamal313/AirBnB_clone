c219da9afeb2a9706940f330a29bca873177a37b
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-08-07 23:02:17 +0300

    updated the class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,9 @@
 import datetime
 import uuid
 
+
 # Define The base model class
-class BaseBodel:
+class BaseModel:
     """Base class for all models"""
 
     # Initialize the instance attributes
@@ -23,4 +24,20 @@ class BaseBodel:
         # Return a formatted string with the class name, id, and __dict__
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
+    def save(self):
+        """Update the updated_at attribute with the current datetime"""
+        # Assign the current datetime to updated at
+        self.updated_at = datetime.datetime.now()
 
+    # Define the to_dict method to return a dictionary representation of the instance
+    def to_dict(self):
+        """Return the dictionary representation of the instance"""
+        # Create a copy of the instance __dict__
+        dict_copy = self.__dict__.copy()
+        # Add key __class__ with the class name of the object
+        dict_copy["__class__"] = self.__class__.__name__
+        # convert the created_at & updated_at to a string iso format
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        # Return the dictionary copy
+        return dict_copy
