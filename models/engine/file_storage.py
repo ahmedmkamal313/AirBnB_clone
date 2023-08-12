0f23a9f7afb1e31e8815589eedfb7af41e1d1c19
Magdalina1 <majdalol95@gmail.com>
2023-08-12 16:00:11 +0300

    update storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,3 +39,64 @@ class FileStorage:
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
+
+
+...
+
+
+class FileStorage:
+    ...
+
+    def _serialize_user(self, obj):
+        """
+        Serialize the User object.
+        """
+        serialized_user = {
+            '__class__': obj.__class__.__name__,
+            'id': obj.id,  # ID of the user
+            'created_at': obj.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),  # Creation timestamp of the user
+            'updated_at': obj.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),  # Update timestamp of the user
+            'email': obj.email,  # Email of the user
+            'password': obj.password,  # Password of the user
+            'first_name': obj.first_name,  # First name of the user
+            'last_name': obj.last_name  # Last name of the user
+        }
+        return serialized_user
+
+    def _deserialize_user(self, serialized_user):
+        """
+        Deserialize the User object.
+        """
+        user = User()
+        user.__dict__.update(serialized_user)
+        user.created_at = datetime.strptime(serialized_user['created_at'],
+                                            "%Y-%m-%dT%H:%M:%S.%f")  # Convert String to datetime
+        user.updated_at = datetime.strptime(serialized_user['updated_at'],
+                                            "%Y-%m-%dT%H:%M:%S.%f")  # Convert String to datetime
+        return user
+
+    def _serialize(self):
+        """
+        Serialize all objects in __objects attribute.
+        """
+        ...
+
+        for obj in objects:
+            if isinstance(obj, User):
+                serialized_obj = self._serialize_user(obj)
+                ...
+
+            ...
+
+    def _deserialize(self):
+        """
+        Deserialize serialized objects from JSON file.
+        """
+        ...
+
+        for class_name, obj_data in serialized_objs.items():
+            if class_name == 'User':
+                obj = self._deserialize_user(obj_data)
+                ...
+
+            ...
