f5079d7da939ed7a55f2f2b2583361c6ea9d3567
Magdalina1 <majdalol95@gmail.com>
2023-08-12 15:33:53 +0300

    user.py

---
+++ models/usre.py
@@ -0,0 +1,8 @@
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    # Define the class attributes
+    email = ""           # Email attribute for the User
+    password = ""        # Password attribute for the User
+    first_name = ""      # First name attribute for the User
+    last_name = ""       # Last name attribute for the User
