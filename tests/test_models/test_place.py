57aeca03b02f4d476166a1c869730f545f99b49e
Magdalina1 <majdalol95@gmail.com>
2023-08-13 10:32:14 -0700

    test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,52 +1,152 @@
-#!/usr/bin/python3
-"""Unittest module for the Place Class."""
+#!/usr/bin/env python3
+"""Test model for Place class"""
 
 import unittest
-import datetime
-import time
-from models.place import Place
-import re
-import json
-from models.engine.file_storage import FileStorage
 import os
-from models import storage
+from models.place import Place
+from datetime import datetime
+from time import sleep
 from models.base_model import BaseModel
+import uuid
 
 
 class TestPlace(unittest.TestCase):
+    """Place model class test case"""
 
-    """Test Cases for the Place class."""
+    @classmethod
+    def setUpClass(cls):
+        """Setup the unittest"""
+        cls.place = Place()
+        cls.place.city_id = str(uuid.uuid4())
+        cls.place.user_id = str(uuid.uuid4())
+        cls.place.name = "Any place in the world"
+        cls.place.description = "Suny Beatch"
+        cls.place.number_rooms = 0
+        cls.place.number_bathrooms = 0
+        cls.place.max_guest = 0
+        cls.place.price_by_night = 0
+        cls.place.latitude = 0.0
+        cls.place.longitude = 0.0
+        cls.place.amenity_ids = []
 
-    def setUp(self):
-        """Sets up test methods."""
-        pass
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up the dirt"""
+        del cls.place
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
+    def test_is_subclass(self):
+        self.assertTrue(issubclass(self.place.__class__, BaseModel))
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+    def checking_for_doc(self):
+        self.assertIsNotNone(Place.__doc__)
 
-    def test_8_instantiation(self):
-        """Tests instantiation of Place class."""
+    def test_has_attributes(self):
+        self.assertTrue('id' in self.place.__dict__)
+        self.assertTrue('created_at' in self.place.__dict__)
+        self.assertTrue('updated_at' in self.place.__dict__)
+        self.assertTrue('city_id' in self.place.__dict__)
+        self.assertTrue('user_id' in self.place.__dict__)
+        self.assertTrue('name' in self.place.__dict__)
+        self.assertTrue('max_guest' in self.place.__dict__)
+        self.assertTrue('price_by_night' in self.place.__dict__)
+        self.assertTrue('latitude' in self.place.__dict__)
+        self.assertTrue('longitude' in self.place.__dict__)
+        self.assertTrue('amenity_ids' in self.place.__dict__)
+        self.assertTrue('description' in self.place.__dict__)
+        self.assertTrue('number_rooms' in self.place.__dict__)
+        self.assertTrue('number_bathrooms' in self.place.__dict__)
 
-        b = Place()
-        self.assertEqual(str(type(b)), "<class 'models.place.Place'>")
-        self.assertIsInstance(b, Place)
-        self.assertTrue(issubclass(type(b), BaseModel))
+    def test_attributes_are_string(self):
+        self.assertIs(type(self.place.city_id), str)
+        self.assertIs(type(self.place.user_id), str)
+        self.assertIs(type(self.place.name), str)
+        self.assertIs(type(self.place.description), str)
+        self.assertIs(type(self.place.number_rooms), int)
+        self.assertIs(type(self.place.max_guest), int)
+        self.assertIs(type(self.place.price_by_night), int)
+        self.assertIs(type(self.place.latitude), float)
+        self.assertIs(type(self.place.longitude), float)
+        self.assertIs(type(self.place.amenity_ids), list)
 
-    def test_8_attributes(self):
-        """Tests the attributes of Place class."""
-        attributes = storage.attributes()["Place"]
-        o = Place()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
+    def test_save(self):
+        self.place.save()
+        self.assertNotEqual(self.place.created_at, self.place.updated_at)
+
+    def test_one_save(self):
+        pl = Place()
+        sleep(0.05)
+        first_updated_at = pl.updated_at
+        pl.save()
+        self.assertLess(first_updated_at, pl.updated_at)
+
+    def test_two_saves(self):
+        pl = Place()
+        sleep(0.05)
+        first_updated_at = pl.updated_at
+        pl.save()
+        second_updated_at = pl.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        pl.save()
+        self.assertLess(second_updated_at, pl.updated_at)
+
+    def test_save_with_arg(self):
+        pl = Place()
+        with self.assertRaises(TypeError):
+            pl.save(None)
+
+class TestPlace_to_dict(unittest.TestCase):
+    """Unittests for testing to_dict method of the Place class."""
+
+    def test_to_dict_type(self):
+        self.assertTrue(dict, type(Place().to_dict()))
+
+    def test_to_dict_contains_correct_keys(self):
+        pl = Place()
+        self.assertIn("id", pl.to_dict())
+        self.assertIn("created_at", pl.to_dict())
+        self.assertIn("updated_at", pl.to_dict())
+        self.assertIn("__class__", pl.to_dict())
+
+    def test_to_dict_contains_added_attributes(self):
+        pl = Place()
+        pl.middle_name = "Holberton"
+        pl.my_number = 98
+        self.assertEqual("Holberton", pl.middle_name)
+        self.assertIn("my_number", pl.to_dict())
+
+    def test_to_dict_datetime_attributes_are_strs(self):
+        pl = Place()
+        pl_dict = pl.to_dict()
+        self.assertEqual(str, type(pl_dict["id"]))
+        self.assertEqual(str, type(pl_dict["created_at"]))
+        self.assertEqual(str, type(pl_dict["updated_at"]))
+
+    def test_to_dict_output(self):
+        dt = datetime.today()
+        pl = Place()
+        pl.id = "123456"
+        pl.created_at = pl.updated_at = dt
+        tdict = {
+            'id': '123456',
+            '__class__': 'Place',
+            'created_at': dt.isoformat(),
+            'updated_at': dt.isoformat(),
+        }
+        self.assertDictEqual(pl.to_dict(), tdict)
+
+    def test_contrast_to_dict_dunder_dict(self):
+        pl = Place()
+        self.assertNotEqual(pl.to_dict(), pl.__dict__)
+
+    def test_to_dict_with_arg(self):
+        pl = Place()
+        with self.assertRaises(TypeError):
+            pl.to_dict(None)
 
 
 if __name__ == "__main__":
