16b8306183a2a98d9db923612128d0f7b4b82338
Magdalina1 <majdalol95@gmail.com>
2023-08-13 10:29:55 -0700

    test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""Unittest module for the City Class."""
+"""Unittest module for the City Class.
+Unittest classes:
+    TestCity_instantiation
+    TestCity_save
+    TestCity_to_dict
+"""
 
 import unittest
-import datetime
+from datetime import datetime
 import time
+import uuid
 from models.city import City
 import re
 import json
@@ -12,41 +18,75 @@ import os
 from models import storage
 from models.base_model import BaseModel
 
-
 class TestCity(unittest.TestCase):
+    """City model class test case"""
 
-    """Test Cases for the City class."""
+    @classmethod
+    def setUpClass(cls):
+        """Setup the unittest"""
+        cls.city = City()
+        cls.city.state_id = str(uuid.uuid4())
+        cls.city.name = "St. Petesburg"
 
-    def setUp(self):
-        """Sets up test methods."""
-        pass
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up the dirt"""
+        del cls.city
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
+    def test_no_args_instantiates(self):
+        self.assertEqual(City, type(City()))
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(City(), storage.all().values())
 
-    def test_8_instantiation(self):
-        """Tests instantiation of City class."""
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(City().id))
 
-        b = City()
-        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
-        self.assertIsInstance(b, City)
-        self.assertTrue(issubclass(type(b), BaseModel))
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(City().created_at))
 
-    def test_8_attributes(self):
-        """Tests the attributes of City class."""
-        attributes = storage.attributes()["City"]
-        o = City()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(City().updated_at))
+
+    def test_is_subclass(self):
+        self.assertTrue(issubclass(self.city.__class__, BaseModel))
+
+    def checking_for_doc(self):
+        self.assertIsNotNone(City.__doc__)
+
+    def test_has_attributes(self):
+        self.assertTrue('id' in self.city.__dict__)
+        self.assertTrue('created_at' in self.city.__dict__)
+        self.assertTrue('updated_at' in self.city.__dict__)
+        self.assertTrue('state_id' in self.city.__dict__)
+        self.assertTrue('name' in self.city.__dict__)
+
+    def test_state_id_is_public_class_attribute(self):
+        ci = City()
+        self.assertEqual(str, type(City.state_id))
+        self.assertIn("state_id", dir(ci))
+        self.assertNotIn("state_id", ci.__dict__)
+
+    def test_name_is_public_class_attribute(self):
+        ci = City()
+        self.assertEqual(str, type(City.name))
+        self.assertIn("name", dir(ci))
+        self.assertNotIn("name", ci.__dict__)
+
+    def test_attributes_are_string(self):
+        self.assertIs(type(self.city.state_id), str)
+        self.assertIs(type(self.city.name), str)
+
+    def test_save(self):
+        self.city.save()
+        self.assertNotEqual(self.city.created_at, self.city.updated_at)
+
+    def test_to_dict(self):
+        self.assertTrue('to_dict' in dir(self.city))
 
 
 if __name__ == "__main__":
