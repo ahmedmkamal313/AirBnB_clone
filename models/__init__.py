072519838015fd31506886e5e3ebda8659102e6e
Magdalina1 <majdalol95@gmail.com>
2023-08-13 14:46:48 -0700

    delete some codes

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
-__authors__ = "Majda Bouzayd, Ahmed Kamal"
 
 
 storage = FileStorage()
